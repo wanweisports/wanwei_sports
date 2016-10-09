c002be8185585f9734361def61956e133f4a98da
2516281 <402942988@qq.com>
2016-10-09 08:29:51 +0800

    new

---
+++ park/src/main/java/com/park/common/bean/GoodInputView.java
@@ -0,0 +1,21 @@
+package com.park.common.bean;
+
+public class GoodInputView extends BaseInputView {
+	
+	private String goodName;
+	private String goodStatus;
+	
+	public String getGoodName() {
+		return goodName;
+	}
+	public void setGoodName(String goodName) {
+		this.goodName = goodName;
+	}
+	public String getGoodStatus() {
+		return goodStatus;
+	}
+	public void setGoodStatus(String goodStatus) {
+		this.goodStatus = goodStatus;
+	}
+	
+}
---
+++ park/src/main/java/com/park/controller/GoodController.java
@@ -0,0 +1,97 @@
+package com.park.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.park.common.bean.GoodInputView;
+import com.park.common.bean.PageBean;
+import com.park.common.bean.ResponseBean;
+import com.park.common.exception.MessageException;
+import com.park.common.po.GoodInfo;
+import com.park.common.po.UserOperator;
+import com.park.common.util.JsonUtils;
+import com.park.service.IGoodService;
+
+@Controller
+@RequestMapping("good")
+public class GoodController extends BaseController {
+	
+	@Autowired
+	private IGoodService goodService;
+	
+	@RequestMapping("updateGood")
+	public String updateGood(Integer goodId, Model model){
+		if(goodId != null){
+			model.addAllAttributes(JsonUtils.fromJsonDF(goodService.getGoodInfo(goodId)));
+		}
+		return "Goods/GoodsInfo";
+	}
+
+	@ResponseBody
+	@RequestMapping("saveGood")
+	public ResponseBean saveGood(GoodInfo goodInfo){
+		try {
+			UserOperator userOperator = super.getUserInfo();
+			goodInfo.setSalesId(userOperator.getId());
+			Integer goodId = goodService.saveGood(goodInfo);
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("goodId", goodId);
+			return new ResponseBean(data);
+		} catch (MessageException e) {
+			e.printStackTrace();
+			return new ResponseBean(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseBean(false);
+		}
+	}
+	
+	@RequestMapping(value = "getGoods")
+	public String getGoods(GoodInputView goodInputView, Model model) {
+		try {
+			model.addAllAttributes(JsonUtils.fromJsonDF(goodInputView));
+			PageBean pageBean = goodService.getGoods(goodInputView);
+			super.setPageInfo(model, pageBean);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Goods/GoodsList";
+	}
+	
+	@ResponseBody
+	@RequestMapping("addGoodCount")
+	public ResponseBean addGoodCount(GoodInfo goodInfo){
+		try {
+			goodService.addGoodCount(goodInfo);
+			return new ResponseBean(true);
+		} catch (MessageException e) {
+			e.printStackTrace();
+			return new ResponseBean(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseBean(false);
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping("goodInOrOut")
+	public ResponseBean goodInOrOut(Integer goodId, Boolean up){
+		try {
+			goodService.updateGoodInOrOut(goodId, up);
+			return new ResponseBean(true);
+		} catch (MessageException e) {
+			e.printStackTrace();
+			return new ResponseBean(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseBean(false);
+		}
+	}
+	
+}
