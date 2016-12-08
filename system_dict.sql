9633531c39ce44374d83f6a2b79937e0e48f3a09
xiaosong.chen@jimubox.com <xiaosong.chen@jimubox.com>
2016-12-08 14:40:38 +0800

    字典项增加

--- system_dict.sql
+++ system_dict.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2016-12-08 14:33:25
+Date: 2016-12-08 14:40:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,7 +28,7 @@ CREATE TABLE `system_dict` (
   `dictAdditional` text,
   PRIMARY KEY (`dictId`),
   KEY `AK_Key_2` (`dictName`,`dictKey`)
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of system_dict
@@ -47,9 +47,11 @@ INSERT INTO `system_dict` VALUES ('41', null, 'SEX', '2', '女', null);
 INSERT INTO `system_dict` VALUES ('50', null, 'BALANCE_STYLE', '1', '现金', null);
 INSERT INTO `system_dict` VALUES ('51', null, 'BALANCE_STYLE', '2', '支付宝', null);
 INSERT INTO `system_dict` VALUES ('52', null, 'BALANCE_STYLE', '3', '微信', null);
-INSERT INTO `system_dict` VALUES ('53', null, 'BALANCE_SERVICE_TYPE', '20', '教师补办会员卡', null);
-INSERT INTO `system_dict` VALUES ('54', null, 'BALANCE_SERVICE_TYPE', '30', '学生补办会员卡', null);
-INSERT INTO `system_dict` VALUES ('55', null, 'BALANCE_SERVICE_TYPE', '100', '场地预订', null);
-INSERT INTO `system_dict` VALUES ('56', null, 'BALANCE_SERVICE_TYPE', '200', '包场预订', null);
-INSERT INTO `system_dict` VALUES ('57', null, 'BALANCE_SERVICE_TYPE', '220', '应收款', null);
-INSERT INTO `system_dict` VALUES ('58', null, 'BALANCE_SERVICE_TYPE', '300', '商品消费', null);
+INSERT INTO `system_dict` VALUES ('53', null, 'BALANCE_STYLE', '4', '银联', null);
+INSERT INTO `system_dict` VALUES ('54', null, 'BALANCE_STYLE', '5', '支票', null);
+INSERT INTO `system_dict` VALUES ('55', null, 'BALANCE_SERVICE_TYPE', '20', '教师补办会员卡', null);
+INSERT INTO `system_dict` VALUES ('56', null, 'BALANCE_SERVICE_TYPE', '30', '学生补办会员卡', null);
+INSERT INTO `system_dict` VALUES ('57', null, 'BALANCE_SERVICE_TYPE', '100', '场地预订', null);
+INSERT INTO `system_dict` VALUES ('58', null, 'BALANCE_SERVICE_TYPE', '200', '包场预订', null);
+INSERT INTO `system_dict` VALUES ('59', null, 'BALANCE_SERVICE_TYPE', '220', '应收款', null);
+INSERT INTO `system_dict` VALUES ('60', null, 'BALANCE_SERVICE_TYPE', '300', '商品消费', null);
